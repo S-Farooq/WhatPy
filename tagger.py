44a4b65545e0556fb1eb44921d62b23597562c05
hammad10 <hammad.mirza@hotmail.com>
2016-02-28 20:53:15 -0500

    Added count_tags function

--- tagger.py
+++ tagger.py
@@ -39,11 +39,23 @@ rlg= re.compile('alhamdu|subhan|astag|^ia$', re.IGNORECASE) #Religious phrases
 corr= re.compile('(.+)\*$', re.IGNORECASE) #Corrections
 emojis = re.compile(u'^\\xf0') #emojis
 
-def count_tags(msg):
+# count_tags(msg) determines the number of times each feature appears in a message.
+# Parameters: A message dictionary containing the message as the key and the feature as the value.
+#             Dictionary of tags that are to be counted.
+# Returns an array containing the total count for each feature.
+def count_tags(msg, tagsDict):
 
+    counts = []
 
-    return
+    maxValue = max(tagsDict.values())
 
+    counts = np.zeros((maxValue + 1))
+
+    for word in msg:
+        if word[1] in tagsDict:
+            counts[tagsDict[word[1]]] += 1
+
+    return counts
 
 def tag_msgs(msg):
     #If the msg is <media omitted> we count that as an entity in itself
@@ -102,6 +114,11 @@ if __name__ == '__main__':
     for ind in range(TRAIN_SIZE):
         X[ind] = tag_msgs(msg_corpus[inds[ind]])
         print X[ind]
+
+        ## count how many times certain tags appear in a message.
+        tags = {'NN': 0, 'IN': 1, 'UH': 2, 'CC': 3, 'RB': 4, 'PRP': 5, 'VB': 6, 'EMJ': 7}
+        tagCount = count_tags( X[ind], tags)
+        print tagCount
     # with open(chat_log, 'rb') as csvfile:
     #     reader = csv.reader(csvfile)
     #     #i is temporary just for testing pusposes so we only look at a certain amount of messages
@@ -116,4 +133,4 @@ if __name__ == '__main__':
     #             break
 
     #print len(train_corpus)
-    #print len(train_labels)
+    #print len(train_lab els)
